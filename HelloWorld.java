14a014ae9f3694ca302308ed77b2b1bddee69bdc
Poya <q262633@gmail.com>
2020-11-08 15:41:16 +0800

    master 修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-		System.out.println("修改文字");
+		System.out.println("master 修改");
         System.out.println("Hello, TEA102的各位, 這邊吃飯好貴...");
     }
 
