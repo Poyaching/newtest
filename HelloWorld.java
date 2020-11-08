b8af6d097a9b548ad5a96b0333f08ae0d859c720
Poya <q262633@gmail.com>
2020-11-08 15:52:27 +0800

    github練習

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+		System.out.println("Github 修改");
 		System.out.println("master 修改");
 		System.out.println("dog 修改");
         System.out.println("Hello, TEA102的各位, 這邊吃飯好貴...");
