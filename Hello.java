b2b14697bc722a7eeeea2a83f496dbbcbdf15fca
Poya <q262633@gmail.com>
2020-11-08 15:35:38 +0800

    修改文字2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+		System.out.println("修改文字2");
 		System.out.println("修改文字");
         System.out.println("Hello, TEA102的各位, 這邊吃飯好貴...");
     }
