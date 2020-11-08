b24b339db556f7c64a620082d47d70f0ccd3037c
Poya <q262633@gmail.com>
2020-11-08 13:45:01 +0800

    把字串刪除

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, TEA102的各位, 這邊吃飯好貴...");
+        System.out.println("Hello, TEA102的各位");
     }
 
 
